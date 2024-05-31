a74c2349817a79df29af1ab3fcd8edf44411da66
Sicheng Zhou <zhousc@uchicago.edu>
2024-05-31 09:41:55 -0700

    update analysis

--- resilience_test/expanse/workflow/run.sh
+++ resilience_test/expanse/workflow/run.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-workflows=("mapreduce")
-failure_rates=("0.3" "0.5" "0.7")
-# failure_types=("dependency")
-failure_types=("dependency" "divide_zero" "environment" "memory" "simple" "ulimit" "walltime")
-repeat=10
+log_file="without.log"
+# workflows=("mapreduce")
+# failure_rates=("0.3" "0.5" "0.7")
+failure_types=("dependency" "divide_zero" "environment" "memory" "ulimit") # "walltime" "simple"
+repeat=5
 
-date > analysis.log
+# log_file="test.log"
+workflows=("mapreduce")
+failure_rates=("0.25")
+# failure_types=("divide_zero")
+# repeat=1
+
+date > $log_file
 
 for workflow in "${workflows[@]}"; do
     for failure_rate in "${failure_rates[@]}"; do
         for failure_type in "${failure_types[@]}"; do
             for i in $(seq 1 $repeat); do
                 current_timestamp_ms=$(date +%s).$(date +%N | awk '{print substr($0,1,3)}')
-                echo "Running $workflow at $current_timestamp_ms with failure rate $failure_rate and failure type $failure_type ($i/$repeat)" >> analysis.log
+                echo "Running $workflow at $current_timestamp_ms with failure rate $failure_rate and failure type $failure_type ($i/$repeat)" >> $log_file
                 python -m webs.run failure-injection \
                     --executor parsl \
                     --true-workflow $workflow \
