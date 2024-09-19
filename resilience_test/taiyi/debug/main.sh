5c0fd342ee7a298f07136bd4fd5baaba00e98124
ClaudiaCumberbatch <12110644@mail.sustech.edu.cn>
2024-09-19 09:35:26 +0800

    update toml files and test scripts

--- resilience_test/taiyi/debug/main.sh
+++ resilience_test/taiyi/debug/main.sh
@@ -2,12 +2,12 @@
 
 source $(conda info --base)/etc/profile.d/conda.sh
 
-# BASES=("cholesky" "docking" "fedlearn" "mapreduce" "moldesign" "montage")
-BASES=("mapreduce")
+BASES=("cholesky" "docking" "fedlearn" "mapreduce" "moldesign" "montage")
+# BASES=("moldesign")
 
 # timeout, random
-# FAILURE_TYPES=("dependency" "failure" "import" "manager-killed" "memory" "ulimit" "worker-killed" "zero-division") 
-FAILURE_TYPES=("worker-killed")
+FAILURE_TYPES=("dependency" "failure" "import" "manager-killed" "memory" "ulimit" "worker-killed" "zero-division") 
+# FAILURE_TYPES=("memory" "ulimit" "worker-killed" "zero-division")
 
 for base in "${BASES[@]}"; do
   for failure_type in "${FAILURE_TYPES[@]}"; do
@@ -27,14 +27,18 @@ for base in "${BASES[@]}"; do
 
     sed -i "s/^failure_type = \".*\"/failure_type = \"$failure_type\"/" "$TOML_FILE"
 
-    echo "当前配置: base = $base, TOML_FILE = $TOML_FILE, failure_type = $failure_type"
+    echo "Start time: $(date), Configuration: base = $base, TOML_FILE = $TOML_FILE, failure_type = $failure_type"
 
-    if [ "$base" == "docking" ]; then
-      # python3 -m taps.run --config "$TOML_FILE" > /dev/null 2>&1
-      python3 -m taps.run --config "$TOML_FILE"
-    else
-      # python -m taps.run --config "$TOML_FILE" > /dev/null 2>&1
-      python -m taps.run --config "$TOML_FILE"
+        if [ "$base" == "docking" ]; then
+          timeout 1800 python3 -m taps.run --config "$TOML_FILE" > /dev/null 2>&1
+          # timeout 1800 python3 -m taps.run --config "$TOML_FILE"
+        else
+          timeout 1800 python -m taps.run --config "$TOML_FILE" > /dev/null 2>&1
+          # timeout 1800 python -m taps.run --config "$TOML_FILE"
+        fi
+    
+    if [ $? -eq 124 ]; then
+        echo "$(date): Python script timed out after 30 minutes."
     fi
   done
 done
